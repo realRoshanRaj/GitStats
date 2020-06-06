166e3d18ea9322763d8f736075b2babd96ab37b0
Roshan Raj <roshan.raj@frogforce503.org>
2020-06-06 12:04:25 -0400

    Doughnut Graph Works

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,11 +1,22 @@
 <template>
   <v-container>
-    <v-col cols="3">
-      <v-card color="transparent">
-        <v-img :src="pfpURL"></v-img>
-        <a class="white--text" target="_blank" :href="userPageURL"><h2>{{name}}</h2></a>
-      </v-card>
-    </v-col>
+    <v-row>
+      <v-col cols="3">
+        <v-card color="transparent">
+          <v-img :src="pfpURL"></v-img>
+          <a class="white--text" target="_blank" :href="userPageURL"><h2>{{name}}</h2></a>
+        </v-card>
+      </v-col>
+<!--      <v-spacer></v-spacer>-->
+      <v-col cols="6">
+        <chartjs-doughnut
+          :bind="true"
+          :datasets="datasets"
+          :labels="labels"
+          :option="{}"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -25,7 +36,14 @@
       pfpURL: '',
       name: '',
       userPageURL: '',
-      data: {}
+      data: {},
+      datasets: [
+        {
+          data: [100, 20, 40],
+          backgroundColor: ["#f36e60", "#ffdb3b", "#185190"],
+        }
+      ],
+      labels: ["Foo", "Bar", "Baz"]
     }),
     methods: {}
 
